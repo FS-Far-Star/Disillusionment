15fce177aebc676dc12151fced3570da5883481f
asdfgjxczsasdf <lj392@cam.ac.uk>
2022-06-22 14:20:02 +0100

    more

--- cat.py
+++ cat.py
@@ -22,7 +22,7 @@ img.save('greyscale.png')
 np_img = np.array(img)
 np_img = np.rot90(np_img, 2)
 np_img = np.fliplr(np_img)
-print(np_img)
+#print(np_img)
 
 # Array back to image
 #Image.fromarray(np_img).show()
@@ -45,10 +45,12 @@ def area(i,j):
     shape = Polygon(zip(x, y))
     return shape.area
 #print(area(1,1))
+
 area_grid = np.zeros((np_img.shape[0],np_img.shape[1]))
 for i in range(0,np_img.shape[0]):
     for j in range(0,np_img.shape[1]):
-        area_grid[i,j] = area(i,j)/A_t
+        area_grid[i,j] = area(i,j)
+area_grid = area_grid/A_t
 #print(area_grid)
 
 '''Image Processing'''
@@ -71,32 +73,43 @@ def cost(a,b):
 loss = area_grid - brightness_comp
 # grad = np.array(np.gradient(loss,width/brightness_comp.shape[0]))
 
-x = np.linspace(0,width,np_img.shape[0])
-y = np.linspace(0,height,np_img.shape[1])
-a,b = np.meshgrid(x, y)
-plt.pcolormesh(a,b,np_img) 
-
 #print(cost(area_grid,brightness_comp))
 
+'''solve poisson'''
+phi = area_grid     #initial guess
 
+def f(phi,i,j): #built-in neunmann boundary condition
+    if i == -1:
+        a = 1
+    elif i == phi.shape[0]:
+        a = phi.shape[0]-1
+    else:
+        a = i
+    if j == -1:
+        b = 1
+    elif j == phi.shape[1]:
+        b = phi.shape[1]-1
+    else:
+        b = j
+    return(phi[a,b])
 
-# u = np.zeros((183,276))
-# v = np.zeros((183,276))
-# for i in range(0,grad.shape[1]):
-#     for j in range(0,grad.shape[2]):
-#         u = grad[0,i,j]
-#         v = grad[1,i,j]
-# print(u)
-# x,y = np.meshgrid(np.linspace(0,loss.shape[0],1),np.linspace(0,loss.shape[1],1))
+for i in range(0,phi.shape[0]-1):
+    for j in range(0,phi.shape[1]-1):
+        delta = f(phi,i-1,j) + f(phi,i+1,j) + f(phi,i,j-1) + f(phi,i,j+1) - 4*f(phi,i,j) + loss[i,j]
+        delta = delta/4*1.94    #1.94 is overcorrection factor
+        phi[i,j] += delta
 
-# plt.quiver(x,y,u,v)
+print(phi)
+
+# print(cost(area_grid,brightness_comp))
+# print(cost(phi,brightness_comp))
+
+# x = np.linspace(0,width,np_img.shape[0])
+# y = np.linspace(0,height,np_img.shape[1])
+# a,b = np.meshgrid(x, y)
+# plt.pcolormesh(a,b,np_img)
 # plt.show()
 
-
-# Field Plotting
-# x = grid.shape[0]
-# y = grid.shape[1]
-# direction = math.sqrt(loss)
 
 
 
